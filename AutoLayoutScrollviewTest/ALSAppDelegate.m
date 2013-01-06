e33dba45a0ffa382773e140991c0b42a1fe54c4c
Alexander von Below <Alex@vonBelow.Com>
2013-01-06 20:34:45 +0100

    OK, let's put some code in …

--- AutoLayoutScrollviewTest/ALSAppDelegate.m
+++ AutoLayoutScrollviewTest/ALSAppDelegate.m
@@ -5,6 +5,9 @@
 //  Created by Alexander v. Below on 06.01.13.
 //  Copyright (c) 2013 Alexander v. Below. All rights reserved.
 //
+// This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+// http://creativecommons.org/licenses/by-sa/3.0/
+//
 
 #import "ALSAppDelegate.h"
 
--- AutoLayoutScrollviewTest/ALSViewController.m
+++ AutoLayoutScrollviewTest/ALSViewController.m
@@ -5,8 +5,19 @@
 //  Created by Alexander v. Below on 06.01.13.
 //  Copyright (c) 2013 Alexander v. Below. All rights reserved.
 //
+// This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+// http://creativecommons.org/licenses/by-sa/3.0/
+//
 
 #import "ALSViewController.h"
+
+// Pictures used are:
+
+// http://www.flickr.com/photos/eren/3963425300/
+// By Erin Emre Kasal
+
+// http://www.flickr.com/photos/ioerror/104101285/
+// By Jacob Appelbaum
 
 @interface ALSViewController ()
 
